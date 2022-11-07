059b1b893e3baf17e57eb9f3b5b8a451c860720d
Singhdevashish <singhdevashishbha@gmail.com>
2022-11-07 11:25:26 +0530

    Update user service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  slides = [
+    {img : "../../assets/img/slide1.jpg"},
+    {img : "../../assets/img/slide2.jpg"},
+    {img : "../../assets/img/slide4.jpg"}
+  ];
 
+	paused = false;
+	
+	@ViewChild('carousel', { static: true }) carousel: NgbCarousel;
+
+	togglePaused() {
+		if (this.paused) {
+			this.carousel.cycle();
+		} else {
+			this.carousel.pause();
+		}
+		this.paused = !this.paused;
+	}
+
+  
 }
---
+++ src/app/users/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient , HttpHeaders} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private baseurl = "http://localhost:3000/users";
+
+  constructor(private http: HttpClient) { }
+
+  fetchUser() {
+    return this.http.get(this.baseurl);
+  }
+
+  deleteUser(id) {
+    return this.http.delete(this.baseurl+"/"+id);
+  }
+
+  postUser(body) {
+    return this.http.post(this.baseurl, body);
+  }
+
+  putUser(body) {
+    return this.http.put(this.baseurl, body);
+  }
+
+}
